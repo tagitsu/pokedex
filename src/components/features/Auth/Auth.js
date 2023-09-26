5fa5dfb69c5e9746d152495ff82324eb40a76a94
Julia Diaków <tagitsu@gmail.com>
2023-09-26 11:24:21 +0200

    Style type effects modal

--- src/components/features/Auth/Auth.js
+++ src/components/features/Auth/Auth.js
@@ -14,8 +14,8 @@ import { doc, setDoc } from 'firebase/firestore';
 
 const Auth = () => {
 
-  const [ registerEmail, setRegisterEmail ] = useState('');
-  const [ registerPassword, setRegisterPassword ] = useState('');
+  // const [ registerEmail, setRegisterEmail ] = useState('');
+  // const [ registerPassword, setRegisterPassword ] = useState('');
   const [ loginEmail, setLoginEmail ] = useState('');
   const [ loginPassword, setLoginPassword ] = useState('');
 
@@ -33,8 +33,8 @@ const Auth = () => {
       console.log('rejestracja rozpoczęta')
       const user = await createUserWithEmailAndPassword(
         auth, 
-        registerEmail, 
-        registerPassword
+        loginEmail, 
+        loginPassword
       );
       console.log('nowy użytkownik', user.user.uid);
       const userAccount = setDoc(doc(db, `users`, `${user.user.uid}`), {
@@ -67,7 +67,7 @@ const Auth = () => {
 
   return(
     <div className='panel'>
-      { !user && 
+      {/* { !user && 
       <div className='panel__registration'>
         <p> Zarejestruj się </p>
         <input 
@@ -80,12 +80,10 @@ const Auth = () => {
         onChange={(event) => {
           setRegisterPassword(event.target.value)
         }} />
-        <Button onClick={register} text='Dodaj użytkownika' />
-      </div>}
+      </div>} */}
 
       { !user && 
       <div className='panel__login'>
-        <p> Zaloguj się </p>
         <input 
         placeholder='Email...' 
         onChange={(event) => {
@@ -98,13 +96,13 @@ const Auth = () => {
         }} />
 
         <Button onClick={login} text='Zaloguj' />
-
+        <Button onClick={register} text='Zarejestruj' />
       </div>}
 
       { user && 
       <div className='panel__account'>
         <p> To jest Pokèdex użytkownika {user.email?.substring(0, user.email.indexOf('@'))} </p>
-        <button onClick={logout} className='panel__logout'> wyloguj </button>
+        <Button onClick={logout} text='wyloguj' />
       </div>}
     </div>
   )
--- src/components/features/TypeEffects/TypeEffects.js
+++ src/components/features/TypeEffects/TypeEffects.js
@@ -45,17 +45,18 @@ const TypeEffects = ({ type, effects, openModal, closeModal }) => {
   return (
     <dialog className='effects' ref={ref} onCancel={closeModal}>
       <header className='effects__header'>
-      <h1 className='effects__title'> {type.name} </h1> 
-      <img src={`${process.env.PUBLIC_URL}/images/types/${type.name}.webp`} alt={`${type.name} type icon`} className='effects__icon'/>
-      <button className='effects__close' onClick={closeModal}> <FontAwesomeIcon icon={faClose} /> </button>
+        <div className='effects__title'>
+          <h1> {type.name} </h1> 
+          <img src={`${process.env.PUBLIC_URL}/images/types/${type.name}.webp`} alt={`${type.name} type icon`} className='effects__icon'/>
+        </div>
+        <button className='effects__close' onClick={closeModal}> <FontAwesomeIcon icon={faClose} /> </button>
       </header>
         <div className='effects__container'>
         {effects?.map( item => 
           <ul key={item[0]} className='effects__relation'>
-            <h2 className='effects__subtitle'> {translate(item[0])} {item[0].includes('from') ? <FontAwesomeIcon icon={faShield} /> : <FontAwesomeIcon icon={faBurst} /> } </h2>
+            <h2 className='effects__subtitle'>  {item[0].includes('from') ? <FontAwesomeIcon icon={faShield} /> : <FontAwesomeIcon icon={faBurst} /> } {translate(item[0])} </h2>
             {item[1].map( value => 
               <li key={value.name} className='effects__types'>
-                {/* {value.name}  */}
                 <img src={`${process.env.PUBLIC_URL}/images/types/${value.name}.webp`} alt={`${type.name} type icon`} className='effects__icon'/>
               </li>)} 
           </ul>
