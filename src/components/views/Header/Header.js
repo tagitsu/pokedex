1f244253a0acd6c6e6b0a611ce1d51f1d3b40d1e
Julia Diaków <tagitsu@gmail.com>
2023-10-02 11:58:47 +0200

    Fix unfinited reads from firebase

--- src/components/views/Header/Header.js
+++ src/components/views/Header/Header.js
@@ -4,23 +4,11 @@ import { db } from '../../../firebase-config';
 import './Header.scss';
 import Auth from '../../features/Auth/Auth';
 
-const Header = ({ user }) => {
-
-  const [ userPokemonsAmount, setUserPokemonsAmount ] = useState();
-
-  useEffect(() => {
-    const getPokemons = async() => {
-      const data = await getDocs(collection(db, 'users', `${user}`, 'pokedex'));
-      setUserPokemonsAmount(data.docs.length);
-    }
-    getPokemons();
-    }, [user]);
+const Header = ({}) => {
 
   return(
     <header className='header'>
-      
-      <Auth pokemonsAmount={userPokemonsAmount} />
-
+      <Auth />
     </header>
   )
 }
