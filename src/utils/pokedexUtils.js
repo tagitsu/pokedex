bb3902041fab1b0982129e1bc5e4d9a8f47e5f73
Julia Diaków <tagitsu@gmail.com>
2023-10-02 10:51:17 +0200

    Add pokecard functions to utils

--- src/utils/pokedexUtils.js
+++ src/utils/pokedexUtils.js
@@ -1,3 +1,21 @@
+import { doc, setDoc } from "firebase/firestore";
+import { db } from '../firebase-config';
+
 const utils = {};
 
+// POKECARD
+utils.nameToUpperCase = (pokemonName) => {
+  return pokemonName[0].toUpperCase() + pokemonName.substring(1);
+};
 
+utils.isInPokedex = (myPokemons, id) => {
+  const pokemonInPokedex = myPokemons.filter( pokemon => pokemon.id == String(id));
+  return (Boolean(pokemonInPokedex.length));
+};
+
+utils.addPokemon = (pokemon, user) => {
+  console.log(pokemon);
+  setDoc(doc(db, `users/${user}/pokedex`, `${pokemon.id}`), pokemon);
+};
+
+export default utils;
