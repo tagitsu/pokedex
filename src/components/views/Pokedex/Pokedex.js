52f5016ff8a9e3d8883f8c5d39cb4e4a88e107f6
Julia Diaków <tagitsu@gmail.com>
2023-10-02 10:57:30 +0200

    Create Search comp

--- src/components/views/Pokedex/Pokedex.js
+++ src/components/views/Pokedex/Pokedex.js
@@ -1,11 +1,12 @@
 import './Pokedex.scss';
 
+
 import { useState, useEffect } from "react";
 import { db } from '../../../firebase-config';
 import { collection, doc, setDoc, getDocs } from 'firebase/firestore';
-import utils from '../../../utils/pokedexUtils';
 import axios from "axios";
 import PokeCard from "../../features/PokeCard/PokeCard";
+import Search from '../../features/Search/Search';
 import Button from '../../common/Button/Button';
 
 const Pokedex = ({ user, allPokemons }) => {
@@ -24,113 +25,114 @@ const Pokedex = ({ user, allPokemons }) => {
     getPokemons();
     }, [user]);
 
-  const searchPokemon = () => {
+  // const searchPokemon = () => {
 
-    // czas wyszukania pokemona/pokemonów
-    const leadingZero = (time) => {
-      return (time < 10) ? '0' + time : time;
-    };
+  //   // czas wyszukania pokemona/pokemonów
+  //   const leadingZero = (time) => {
+  //     return (time < 10) ? '0' + time : time;
+  //   };
     
-    const date = new Date();
-    const captureTime = `${leadingZero(date.getDate())}.${leadingZero(date.getMonth() +1)}.${date.getFullYear()} ${leadingZero(date.getHours())}:${leadingZero(date.getMinutes())}:${leadingZero(date.getSeconds())}`;
+  //   const date = new Date();
+  //   const captureTime = `${leadingZero(date.getDate())}.${leadingZero(date.getMonth() +1)}.${date.getFullYear()} ${leadingZero(date.getHours())}:${leadingZero(date.getMinutes())}:${leadingZero(date.getSeconds())}`;
 
-      // znalezienie pasujących do wyszukiwania
-      const searchMatch = () => {
-        const matchingPokemons = allPokemons.filter( pokemon => pokemon.name.includes(search) );
-        console.log('pasujące pokemony', search, matchingPokemons.length);
-        for ( let i = 0; i < matchingPokemons.length; i++) {
-          let pokemon, pokemonTypes = [], pokemonAbilities = []
+  //     // znalezienie pasujących do wyszukiwania
+  //     const searchMatch = () => {
+  //       const matchingPokemons = allPokemons.filter( pokemon => pokemon.name.includes(search) );
+  //       console.log('pasujące pokemony', search, matchingPokemons.length);
+  //       for ( let i = 0; i < matchingPokemons.length; i++) {
+  //         let pokemon, pokemonTypes = [], pokemonAbilities = []
   
-          axios.get(matchingPokemons[i].url).then( response => {
-            const pokemonObject = response.data;
-            console.log(`pokemonObject ${pokemonObject.name}`, pokemonObject);
+  //         axios.get(matchingPokemons[i].url).then( response => {
+  //           const pokemonObject = response.data;
+  //           console.log(`pokemonObject ${pokemonObject.name}`, pokemonObject);
   
-            for (let i = 0; i < pokemonObject.types.length; i++) {
-              pokemonTypes.push({
-                name: pokemonObject?.types[i].type.name,
-                url: pokemonObject?.types[i].type.url,
-              })
-            }
+  //           for (let i = 0; i < pokemonObject.types.length; i++) {
+  //             pokemonTypes.push({
+  //               name: pokemonObject?.types[i].type.name,
+  //               url: pokemonObject?.types[i].type.url,
+  //             })
+  //           }
     
-            for (let i = 0; i < pokemonObject.abilities.length; i++) {
-              pokemonAbilities.push(pokemonObject?.abilities[i].ability.name)
-            }
+  //           for (let i = 0; i < pokemonObject.abilities.length; i++) {
+  //             pokemonAbilities.push(pokemonObject?.abilities[i].ability.name)
+  //           }
   
-            const getSpeciesInfo = () => {
-              if (pokemonObject) {
-                axios.get(pokemonObject.species.url).then( response => {
-                  const pokemonSpecies = response.data;
-                  const funFacts = pokemonSpecies.flavor_text_entries.filter( text => text.language.name === 'en');
+  //           const getSpeciesInfo = () => {
+  //             if (pokemonObject) {
+  //               axios.get(pokemonObject.species.url).then( response => {
+  //                 const pokemonSpecies = response.data;
+  //                 const funFacts = pokemonSpecies.flavor_text_entries.filter( text => text.language.name === 'en');
                   
-                  const pickText = (funFacts) => {
-                    const index = Math.floor(Math.random() * funFacts.length);
-                    console.log(index, funFacts[index].flavor_text);
-                    return funFacts[index]?.flavor_text;
-                  }
+  //                 const pickText = (funFacts) => {
+  //                   const index = Math.floor(Math.random() * funFacts.length);
+  //                   console.log(index, funFacts[index].flavor_text);
+  //                   return funFacts[index]?.flavor_text;
+  //                 }
     
-                  pokemon = { 
-                    ...pokemon, 
-                    habitat: pokemonSpecies.habitat?.name ? pokemonSpecies.habitat.name : null, 
-                    color: pokemonSpecies.color?.name ? pokemonSpecies.color.name : null,
-                    text: pickText(funFacts)
-                  }
+  //                 pokemon = { 
+  //                   ...pokemon, 
+  //                   habitat: pokemonSpecies.habitat?.name ? pokemonSpecies.habitat.name : null, 
+  //                   color: pokemonSpecies.color?.name ? pokemonSpecies.color.name : null,
+  //                   text: pickText(funFacts)
+  //                 }
 
-                  console.log(`etap II - obiekt pokemon ${pokemonObject.id}`, pokemon);
-                  setPokemonData(pokemonData => [...pokemonData, pokemon]);
-                  //setMyPokemon();
+  //                 console.log(`etap II - obiekt pokemon ${pokemonObject.id}`, pokemon);
+  //                 setPokemonData(pokemonData => [...pokemonData, pokemon]);
+  //                 //setMyPokemon();
             
-                }) 
-              }
+  //               }) 
+  //             }
     
-              pokemon = {
-                id: pokemonObject.id,
-                name: pokemonObject.name,
-                types: pokemonTypes,
-                appearance: {
+  //             pokemon = {
+  //               id: pokemonObject.id,
+  //               name: pokemonObject.name,
+  //               types: pokemonTypes,
+  //               appearance: {
       
-                  image: pokemonObject.sprites.other.dream_world.front_default, // pokemonObject.sprites.front_default - ścieżka do grafik pikselowych
-                  image2: pokemonObject.sprites.other.home.front_default,
-                  image3: pokemonObject.sprites.front_default,
-                  height: pokemonObject.height / 10,
-                  weight: pokemonObject.weight /10,
-                },
-                stats: {
-                  hp: pokemonObject.stats[0].base_stat,
-                  atk: pokemonObject.stats[1].base_stat,
-                  def: pokemonObject.stats[2].base_stat,
-                  spAtk: pokemonObject.stats[3].base_stat,
-                  spDef: pokemonObject.stats[4].base_stat,
-                  spd: pokemonObject.stats[5].base_stat,
-                },
-                abilities: pokemonAbilities,
-                captureTime: captureTime,
-              };
-              console.log(`etap I - obiekt pokemon ${pokemon.name}`, pokemon);
-            }
-            getSpeciesInfo();
-          })
-        }
-      }
-      searchMatch();
+  //                 image: pokemonObject.sprites.other.dream_world.front_default, // pokemonObject.sprites.front_default - ścieżka do grafik pikselowych
+  //                 image2: pokemonObject.sprites.other.home.front_default,
+  //                 image3: pokemonObject.sprites.front_default,
+  //                 height: pokemonObject.height / 10,
+  //                 weight: pokemonObject.weight /10,
+  //               },
+  //               stats: {
+  //                 hp: pokemonObject.stats[0].base_stat,
+  //                 atk: pokemonObject.stats[1].base_stat,
+  //                 def: pokemonObject.stats[2].base_stat,
+  //                 spAtk: pokemonObject.stats[3].base_stat,
+  //                 spDef: pokemonObject.stats[4].base_stat,
+  //                 spd: pokemonObject.stats[5].base_stat,
+  //               },
+  //               abilities: pokemonAbilities,
+  //               captureTime: captureTime,
+  //             };
+  //             console.log(`etap I - obiekt pokemon ${pokemon.name}`, pokemon);
+  //           }
+  //           getSpeciesInfo();
+  //         })
+  //       }
+  //     }
+  //     searchMatch();
     
-  };
+  // };
 
-  const searchMyPokemon = () => {
-    const myPokemon = userPokemons.filter( 
-      pokemon => 
-      pokemon.name.includes(search) 
-      || 
-      pokemon.id === search 
-    );
-    setMyPokemon(myPokemon);
-  };
+  // const searchMyPokemon = () => {
+  //   const myPokemon = userPokemons.filter( 
+  //     pokemon => 
+  //     pokemon.name.includes(search) 
+  //     || 
+  //     pokemon.id === search 
+  //   );
+  //   setMyPokemon(myPokemon);
+  // };
 
-  // TODO Po dodaniu pokemona do mojego Pokedexu chciałabym aby widok odświeżał się od razu.
-  const addPokemon = (id) => {
-    const [ pokemonToPokedex ] = pokemonData.filter( pokemon => pokemon.id === id);
-    setDoc(doc(db, `users/${user}/pokedex`, `${id}`), pokemonToPokedex);
-    //window.location.reload();
-  };
+  // const addPokemon = (e, id) => {
+  //   e.preventDefault();
+  //   const [ pokemonToPokedex ] = pokemonData.filter( pokemon => pokemon.id === id);
+  //   setDoc(doc(db, `users/${user}/pokedex`, `${id}`), pokemonToPokedex);
+  //   setPokemonData([]);
+  //   window.location.reload();
+  // };
 
   const getFirstLetters = () => {
     const allLetters = userPokemons.map( pokemon => pokemon.name[0] );
@@ -168,9 +170,9 @@ const Pokedex = ({ user, allPokemons }) => {
 
   return (
     <div className='pokedex'>
-      <div className='pokedex__search'>
+      {/* <div className='pokedex__search'>
         <div className='pokedex__inputs'>
-          <input type='text' onChange={(event) => setSearch(event.target.value)}/>
+          <input type='text' onChange={(event) => setSearch(event.target.value)} defaultValue={search} />
           <Button onClick={searchPokemon} text='Search the Pokèmon database'/>
           <Button onClick={searchMyPokemon} text='Search my Pokèdex'/>
         </div>
@@ -186,9 +188,12 @@ const Pokedex = ({ user, allPokemons }) => {
               <PokeCard key={pokemon.id} pokemon={pokemon} myPokemons={userPokemons}/>)
           }
         </div>
-      </div>
+      </div> */}
+      <Search user={user} allPokemons={allPokemons} userPokemons={userPokemons} />
       <div className='pokedex__menu'>
-        <p> Filter your Pokèdex </p>
+        <section className='pokedex__menu--letters'>
+          <button className='pokedex__button' onClick={(e) => setSortedPokemons(userPokemons)}> all pokemons </button>
+        </section>
         <section className='pokedex__menu--letters'>
           { getFirstLetters().map( letter => <button className='pokedex__button' key={letter} onClick={(e) => getPokemonsByName(e.target.innerText)}> {letter.toUpperCase()} </button>) }
         </section>
