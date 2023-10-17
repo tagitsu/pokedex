3c6690136e0f7dff0ae2a6dc27e86b8a191714d1
Julia Diaków <tagitsu@gmail.com>
2023-10-17 13:38:33 +0200

    Fix display cards in pokedex screen

--- src/components/features/LogoBtn/LogoBtn.js
+++ src/components/features/LogoBtn/LogoBtn.js
@@ -1,13 +1,10 @@
 import './LogoBtn.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
 import utils from '../../../utils/pokedexUtils';
 
 const LogoBtn = () => {
 
   return (
     <button className='logo' onClick={utils.reloadPokedex}>
-      <FontAwesomeIcon icon={faArrowsRotate} />
     </button>
   )
 };
