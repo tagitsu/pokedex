9140e19149d018016c3b853c7c08bd0c1acb0e71
Julia Diaków <tagitsu@gmail.com>
2023-10-19 13:28:08 +0200

    Add btns next to logo

--- src/components/features/LogoBtn/LogoBtn.js
+++ src/components/features/LogoBtn/LogoBtn.js
@@ -1,11 +1,16 @@
 import './LogoBtn.scss';
 import utils from '../../../utils/pokedexUtils';
+import clsx from 'clsx';
 
 const LogoBtn = () => {
 
   return (
-    <button className='logo' onClick={utils.reloadPokedex}>
-    </button>
+    <div className='logo'>
+      <button className='logo__reload' onClick={utils.reloadPokedex}></button>
+      <button className={clsx('logo__btn', 'logo__btn--red')}></button>
+      <button className={clsx('logo__btn', 'logo__btn--yellow')}></button>
+      <button className={clsx('logo__btn', 'logo__btn--green')}></button>
+    </div>
   )
 };
 
