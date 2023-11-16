74e89abcde17f49a87a57b9fb2ce4fcb9e37b1eb
Julia Diaków <tagitsu@gmail.com>
2023-11-16 15:07:48 +0100

    Style pokemon component

--- src/components/features/Pokemon/Pokemon.js
+++ src/components/features/Pokemon/Pokemon.js
@@ -1,8 +1,11 @@
 import './Pokemon.scss';
 import clsx from 'clsx';
 import utils from '../../../utils/pokedexUtils';
+import { useState } from 'react';
 
 const Pokemon = ({ pokemon, chooseCard }) => {
+
+  const [ isVisible, setIsVisible ] = useState(false);
 
   const img = () => { 
     if ( pokemon.appearance.image ) { 
@@ -14,8 +17,23 @@ const Pokemon = ({ pokemon, chooseCard }) => {
     }
   };
 
+
+  // return(
+  //   <button className={clsx('pokemon', `pokemon__${pokemon.types[0].name}`)} onClick={() => chooseCard(pokemon.id)}>
+  //     <div className={clsx('pokemon__image')}>
+  //       <img src={img()} alt={pokemon.name} />
+  //     </div>
+  //   </button>
+  // );
+
+  // onMouseEnter={() => setIsRolling(true)} onMouseLeave={() => setIsRolling(false)}
+
   return(
-    <button className={clsx('pokemon', `pokemon__${pokemon.types[0].name}`)} onClick={() => chooseCard(pokemon.id)}>
+    <button className={clsx('pokemon')} onClick={() => chooseCard(pokemon.id)} onMouseEnter={() => setIsVisible(true)} onMouseLeave={() => setIsVisible(false)}>
+      <div className={clsx('pokemon__pokeball')}/>
+      <h2 className={clsx(isVisible && 'pokemon__name', `pokemon__${pokemon.types[0].name}`)}>
+        {pokemon.name} <span>#{pokemon.id}</span>
+      </h2>
       <div className={clsx('pokemon__image')}>
         <img src={img()} alt={pokemon.name} />
       </div>
